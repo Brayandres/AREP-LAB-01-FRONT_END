37675b13a34ac5032a25d40ff6e14cf7e3dccde2
Brayan Macías <brayandresmt@outlook.com>
2022-08-28 22:41:40 -0500

    Table head

--- src/components/ResultsTable.js
+++ src/components/ResultsTable.js
@@ -1,5 +1,7 @@
 function ResultsTable(props) {
 
+	let wasProcesed = false;
+
 	const {
 		data = {},
 		specificProperty = ""
@@ -25,7 +27,13 @@ function ResultsTable(props) {
 				}
 				object = object[lastKey];
 			}
-			if (object != null) return object;
+			if (object != null) {
+				wasProcesed = true;
+				return object;
+			}
+			else {
+				return {};
+			}
 		}
 		else {
 			return object;
@@ -35,9 +43,43 @@ function ResultsTable(props) {
     const iterableData = searchProperty(data, specificProperty);
 
     const fillDataInTable = object => {
-        console.log("\n   Length: " + Object.keys(object).length);
-		console.log("    PROCESED:\n" + Object.values(object));
-        return <p>ZZZZZ....</p>;
+        //console.log("\n   Length: " + Object.keys(object).length);
+		//console.log("    PROCESED:\n" + Object.values(object));
+		if (wasProcesed) {
+			const keys = Object.keys(object);
+			const values = Object.values(object);
+
+			return (
+				<table>
+					<thead>
+						<tr>
+							<th>Period</th>
+							{Object.keys(values[0]).map(tittle => 
+								<th>{tittle.slice(tittle.indexOf(" ") + 1)}</th>
+							)}
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>2</td>
+							<td>3</td>
+							<td>4</td>
+							<td>5</td>
+							<td>¿</td>
+						</tr>
+						<tr>
+							<td>6</td>
+							<td>7</td>
+							<td>8</td>
+							<td>9</td>
+							<td>0</td>
+							<td>?</td>
+						</tr>
+					</tbody>
+				</table>
+			);
+		}
     };
 
     return (
