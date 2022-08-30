7c6b43536caa2bed4a6c10adab3b5c6ab8726c45
Brayan Macías <brayandresmt@outlook.com>
2022-08-30 15:16:29 -0500

    Adding API specification

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
     const makeRequest = async () => {
         const PARAMS = "?symbol=" + stockSymbol + "&function=" + timeframeValue + "&interval=" + timeIntervalValue;
-        let request = await fetch(PATH + PARAMS);
+        let request = await fetch("http://localhost:4567" + PATH + PARAMS);
         let response = await request.json();
         setResponseData(response);
     };
@@ -58,9 +58,27 @@ function App() {
     return (
         <PageWrapper>
             <TopBar/>
-            <p id='initial-message'>
-                The following service allows you to check the current status of the stock market based on the parameters you configure to obtain information.
-            </p>
+            <div id='top-text-container'>
+                <p id='initial-message'>
+                    The following service allows you to check the current status of the stock market based on the parameters you configure
+                    to obtain information. The meaning of each of the parameters is explained in more detail below.
+                </p>
+                <p className='parameter-especification'>
+                    <b>Stock Symbol:</b><br/>A stock symbol is an abbreviation that refers to a company's share listed on stock exchanges.
+                    It is a unique code comprising a set of characters, i.e., letters, numbers, or a combination of both. It is used to distinguish
+                    a particular stock from the other securities.
+                </p>
+                <p className='parameter-especification'>
+                    <b>Timeframe:</b><br/>Represents the time series or periodicity of the information to be obtained.
+                </p>
+                <p className='parameter-especification'>
+                    <b>TimeInterval*:</b><br/>Represents the elapsed time interval between two consecutive data points in the time series. Each
+                    datum retains the same time distance with each consecutive datum.
+                </p>
+                <p className='parameter-especification' id='note-about-section-timeinterval'>
+                    <b>(* TimeInterval will only be available for the <i>INTRA_DAY</i> option of Timeframe.)</b>
+                </p>
+            </div>
             <input type="text" placeholder="Input a stock sysmbol" value={stockSymbol}
                 onChange={event => setStockSymbol(event.target.value)}
             />
